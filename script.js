8a9f14fc992e46cb5c83514684832ba580822b3f
maxine-janka <159133549+maxine-janka@users.noreply.github.com>
2025-02-02 20:14:30 +1100

    Add task function linked to add button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // SELECTORS
 const taskInput = document.querySelector('.form-enter-task')
 const taskAddButton = document.querySelector('.form-add-button')
-const toDoListItems = document.querySelector('.task-tile-container')
+const toDoListItems = document.querySelector('.task-tile-wrapper')
 
 //EVENT LISTENERS
 taskAddButton.addEventListener('click', addToDoItem);
@@ -11,21 +11,47 @@ function addToDoItem(event){
     event.preventDefault();
 
     const addItemDiv = document.createElement('div');
-    addItemDiv.classList.add('task-tile-conatiner');
+    addItemDiv.classList.add('task-tile-container');
+
+    //Container for task text and category
+    const taskAndCategoryContainer = document.createElement('div')
+    taskAndCategoryContainer.classList.add('task-tile-text-container')
+    addItemDiv.appendChild(taskAndCategoryContainer);
     
-    const addItemLi = document.createElement('li');
-    addItemLi.innerText = 'New Task';
-    addItemLi.classList.add('.task-tile-container')
-    addItemDiv.appendChild(addItemLi);
+    //Add task category
+    const addCatgeory = document.createElement('div');
+    addCatgeory.innerText = 'Urgent';
+    addCatgeory.classList.add('task-tile-category');
+    taskAndCategoryContainer.appendChild(addCatgeory);
+    
+    //Add task text
+    const addItem = document.createElement('div');
+    addItem.innerText = 'Pay phone bill';
+    addItem.classList.add('task-tile-entered-task')
+    taskAndCategoryContainer.appendChild(addItem);
+
+    
+    
+    //Add container for checkbox and delete button
+    const doneAndDeleteContainer = document.createElement('div')
+    doneAndDeleteContainer.classList.add('task-tile-checkbox-container');
+    addItemDiv.appendChild(doneAndDeleteContainer);
+    
     //Add checkbox
-    const completedCheckBox = document.createElement('div');
-    completedCheckBox.innerHTML =  '<input type="checkbox"></input>';
-    addItemDiv.appendChild(completedCheckBox);
+    const divCompleteCheckBox = document.createElement('div')
+    divCompleteCheckBox.classList.add('task-tile-checkbox-done');
+    const completedCheckBox = document.createElement('input');
+    completedCheckBox.type = 'checkbox';
+    divCompleteCheckBox.append(completedCheckBox);
+    doneAndDeleteContainer.appendChild(divCompleteCheckBox);
     
+    //Add Delete button
     const deleteItemButton = document.createElement('div');
     deleteItemButton.innerText =  'x';
     deleteItemButton.classList.add('task-tile-remove');
-    addItemDiv.appendChild(deleteItemButton);
-
+    doneAndDeleteContainer.appendChild(deleteItemButton);
+    
     toDoListItems.appendChild(addItemDiv);
+
+    
 };
